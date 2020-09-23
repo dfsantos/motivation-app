4c46503affc5df66c5fd50a1f7f3bd54a63dfad0
Diego Santos <santos.diegoferreira@gmail.com>
2020-09-23 15:31:16 -0300

    feat: Create splash activity

---
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.motivation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+    }
+
+}
